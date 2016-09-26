0384fd4029ba6ee5af2d664678b84531e0d7339f
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2016-09-26 12:24:56 +0700

    Setup project.

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '9.0'
+
+target 'firebase authentication' do
+  use_frameworks!
+  
+  pod 'Firebase'
+
+end
---
+++ Pods/Target Support Files/Pods-firebase authentication/Pods-firebase authentication-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_firebase_authenticationVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_firebase_authenticationVersionString[];
+
