fa6b88d6ed7bc7234673e7b569778b8cc14508b5
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2017-02-13 14:11:23 +0700

    Create select login's method layout.

---
+++ firebase authentication/Constants/LoginMethods.swift
@@ -0,0 +1,18 @@
+//
+//  LoginMethods.swift
+//  firebase authentication
+//
+//  Created by Kittisak Phetrungnapha on 2/13/2560 BE.
+//  Copyright © 2560 Kittisak Phetrungnapha. All rights reserved.
+//
+
+import Foundation
+
+enum LoginMethods: String {
+    case email = "Email"
+    case facebook = "Facebook"
+    case google = "Google"
+    case twitter = "Twitter"
+    case github = "Github"
+    case anonymous = "Anonymous"
+}
--- firebase authentication/SelectLoginMethodsViewController.swift
+++ firebase authentication/SelectLoginMethodsViewController.swift
@@ -12,10 +12,43 @@ class SelectLoginMethodsViewController: UIViewController {
 
     // MARK: - Properties
     @IBOutlet weak var tableView: UITableView!
+    fileprivate let cellIdentifier = "cell"
+    fileprivate var dataSource: [String]!
     
-    // MARK: - View controller's life cycle    
+    // MARK: - View controller's life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        dataSource = [LoginMethods.email.rawValue,
+                      LoginMethods.facebook.rawValue,
+                      LoginMethods.google.rawValue,
+                      LoginMethods.twitter.rawValue,
+                      LoginMethods.github.rawValue,
+                      LoginMethods.anonymous.rawValue]
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        tableView.isScrollEnabled = false
     }
 
+}
+
+extension SelectLoginMethodsViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) else { return UITableViewCell() }
+        cell.textLabel?.text = dataSource[indexPath.row]
+        return cell
+    }
+    
+}
+
+extension SelectLoginMethodsViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
 }
